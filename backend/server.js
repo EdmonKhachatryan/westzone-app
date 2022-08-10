197676afc858a578c438b06054b6ba769d6adb15
Edmon-black-17 <universaltruth002@gmail.com>
2022-08-10 13:17:04 -0700

    Publish To Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import SocketIO from 'socket.io';
+import { Server } from 'socket.io';
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -44,7 +44,7 @@ app.use((err, req, res, next) => {
 const port = process.env.PORT || 5000;
 
 const httpServer = http.Server(app);
-const io = new SocketIO(httpServer, { cors: { origin: '*' } });
+const io = new Server(httpServer, { cors: { origin: '*' } });
 const users = [];
 
 io.on('connection', (socket) => {
